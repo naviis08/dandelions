d421d7c490c4d658c5a22c7e231aa6376a874470
naviis08 <shiilisa8@gmail.com>
2025-04-19 13:25:46 +0530

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,5 +1,20 @@
 # let us begin with building a simple quiz
 
 print("welcome to the hunger games quiz!")
-username = input("Enter your game name")
+username = input("Enter your game name") # user will enter the username used throughout the game in the variable username
 print("You will be" + username)
+from tkinter import *        
+
+
+from tkinter.ttk import *
+
+root = Tk() 
+  
+root.geometry('100x100')     
+
+btn = Button(root, text = 'Click me !', 
+                command = root.destroy) 
+
+btn.pack(side = 'top')     
+
+root.mainloop() 
